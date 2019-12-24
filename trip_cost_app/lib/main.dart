5fe667f707c34bae996a94762f30736729bf068e
Chris Zhou （周昆） <Chris_K_Zhou@manulife.com>
2019-12-24 18:01:56 +0800

    update state in trip cost app

---
+++ trip_cost_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello You',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloYou(),
+    );
+  }
+}
+
+class HelloYou extends StatefulWidget {
+  @override
+  _HelloYouState createState() => _HelloYouState();
+}
+
+class _HelloYouState extends State<HelloYou> {
+  String name = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15.0),
+        child: Column(children: <Widget>[
+          TextField(
+            onChanged: (String string) {
+              setState(() {
+                name = string;
+              });
+            },
+          ),
+          Text("Hello" + name)
+        ]),
+      ),
+    );
+  }
+}

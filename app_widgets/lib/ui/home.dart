6c858efc7f26679d18676c8e06e8b230edde20d7
Chris Zhou （周昆） <Chris_K_Zhou@manulife.com>
2019-12-24 17:14:47 +0800

    update the widgets files

--- app_widgets/lib/ui/home.dart
+++ app_widgets/lib/ui/home.dart
@@ -4,14 +4,71 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      color: Colors.deepOrangeAccent,
-      width: 192.0,
-      height: 50.0,
-      child: Text(
-        "Pizza",
-        textDirection: TextDirection.ltr,
+        alignment: Alignment.center,
+        color: Colors.deepOrangeAccent,
+        padding: EdgeInsets.only(top: 30.0, left: 10.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text("vokda, Tomato, Mozzi, Bread",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal)),
+                ),
+                Expanded(
+                  child: Text("vokda, Tomato, Mozzi, Bread",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          fontSize: 30.0,
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Oxygen',
+                          fontWeight: FontWeight.normal)),
+                )
+              ],
+            ),
+            BearImageWidget(),
+            OrderButton(),
+          ],
+        ));
+  }
+}
+
+class BearImageWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    AssetImage bearAssert = AssetImage('images/bear.png');
+    Image image = Image(image: bearAssert, width: 400.0, height: 400.0);
+    return Container(child: image);
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var button = Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: RaisedButton(
+        child: Text('Order Your Pizza'),
+        color: Colors.lightGreen,
+        elevation: 5.0,
+        onPressed: () {
+          order(context);
+        },
       ),
     );
+    return button;
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
   }
 }
